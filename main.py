03a44b83271c7ac9401ae0bf4d841aeec176be46
28bistai <28bistai@bwschools.net>
2025-03-07 11:23:51 -0500

    commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import turtle
+screen = turtle.Screen()
+screen.screensize(500,500)
+t = turtle.Turtle()
+screen.bgcolor('tan')
+
+t.penup()
+t.goto(-80,15)
+t.pendown()
+t.pencolor('blue')
+t.circle(35)
+
+t.penup()
+t.goto(0,15)
+t.pendown()
+t.pencolor('black')
+t.circle(35)
+
+t.penup()
+t.goto(80,15)
+t.pendown()
+t.pencolor('red')
+t.circle(35)
+
+t.penup()
+t.goto(-40,-15)
+t.pendown()
+t.pencolor('yellow')
+t.circle(35)
+
+t.penup()
+t.goto(40,-15)
+t.pendown()
+t.pencolor('green')
+t.circle(35)
+
+t.penup()
+t.goto(0,100)
+t.pencolor('purple')
+t.write("Winter Olympics", font=("Arial", 30,"bold" ), align="center")
+
+t.penup()
+t.goto(0,-100)
+t.pencolor('purple')
+t.write("2026", font=("Arial", 30,"bold" ), align="center")
+turtle.done()
